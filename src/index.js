8c0b381de9f78f60839347a0ba0473eb34f3ed56
uncanny-vegan-taco <kylihumphreys@gmail.com>
2023-11-14 16:46:55 -0800

    small changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import Content from "./Content";
-import "./styles.css";
+import "./Styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
